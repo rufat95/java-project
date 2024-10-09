794bc42eac43e99d3f327d625abea044c6365564
rufat <rufat.hesenzade.95@gmail.com>
2024-10-09 16:36:47 +0400

    lesson3

--- lesson3/src/Main.java
+++ lesson3/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int eded = 10;
+
+        for (int i = 1; i <= eded; i++) {
+            int sadeBolen = eded % i;
+            if (sadeBolen == 0){
+                System.out.println(i);
+            }
+        }
+
+        
     }
 }
