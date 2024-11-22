29fcfe116325dceb1e1baee9686d658c69827101
rufat <rufat.hesenzade.95@gmail.com>
2024-11-22 12:36:56 +0400

    New codes 11.22.2024

--- Arrays/src/Task11.java
+++ Arrays/src/Task11.java
@@ -1,12 +1,16 @@
 public class Task11 {
     public static void main(String[] args) {
-        int[] arr1 = new int[]{1, 2, 3, 4, 5};
-        int[] arr2 = new int[]{1, 2, 3, 4, 5, 6};
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6};
+        int[] arr2 = new int[]{1, 2, 3, 5, 6};
 
-        int count = 0;
-//        for (int i = 0; i < arr1.length ; i++) {
-//
-//        }
-        System.out.println(count);
+        int countArr1 = 0;
+        int countArr2 = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            countArr1 += arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            countArr2 += arr2[i];
+        }
+        System.out.println(countArr1 - countArr2);
     }
 }
